5275384c9e17ec8d7f1018caec13920ecaf755b5
Sezgin Ererman <sezginererman@gmail.com>
2021-04-18 19:36:42 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionaryClass<int, string> product = new MyDictionaryClass<int, string>();
+            product.Add(1, "Elma");
+            product.Add(2, "Armut");
+            product.Add(3, "Karpuz");
+            product.Add(4, "Üzüm");
+            product.Add(5, "Çilek");
+
+
+            //Console.WriteLine(dictionary1.keysLength);
+
+            for (int i = 0; i < product.keysLength; i++)
+            {
+                Console.WriteLine("Ürün Adı : " + product.Values[i] + "                  Barkod No : " + product.Keys[i]);
+            }
+        }
+    }
+}
